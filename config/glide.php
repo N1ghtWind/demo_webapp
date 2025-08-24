4251332dbb15f4142f7ba7cbbcdf789a44987d61
Terezsak Jozsef <jozseft@digitalarcher.dev>
2025-08-24 23:41:37 +0200

    Feat (config): Added config, files, and update existing config

---
+++ config/glide.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'signature' => env('GLIDE_SIGNATURE'),
+    'source' => [
+        'driver'   => 's3',
+        'key'      => env('AWS_ACCESS_KEY_ID'),
+        'secret'   => env('AWS_SECRET_ACCESS_KEY'),
+        'region'   => env('AWS_REGION'),
+        'bucket'   => env('AWS_BUCKET'),
+        'url'      => env('AWS_S3_URL', ''),
+        'options'  => [],
+    ],
+    'cache' => [
+        'driver' => 'local',
+    ],
+];
