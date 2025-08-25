f3c2ab24b06e657839f70a681d99bb40e606ff54
Terezsak Jozsef <jozseft@digitalarcher.dev>
2025-08-25 15:25:53 +0200

    Feat: bind repository interfaces to implementations in service container

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,25 @@
 
 namespace App\Providers;
 
+use App\Repositories\AdminAuthRepository;
+use App\Repositories\Interfaces\AdminUserAuthenticationInterface;
+use App\Repositories\Interfaces\MediaRepositoryInterface;
+use App\Repositories\Interfaces\UserRegistrationInterface;
+use App\Repositories\MediaRepository;
+use App\Repositories\UserRegistrationRepository;
 use Illuminate\Support\ServiceProvider;
+use App\Repositories\Interfaces\UserRepositoryInterface;
+use App\Repositories\UserRepository;
+use App\Repositories\Interfaces\ProductRepositoryInterface;
+use App\Repositories\ProductRepository;
+use App\Repositories\Interfaces\OrderRepositoryInterface;
+use App\Repositories\OrderRepository;
+use App\Repositories\Interfaces\OrderItemRepositoryInterface;
+use App\Repositories\OrderItemRepository;
+use App\Repositories\Interfaces\CategoryRepositoryInterface;
+use App\Repositories\CategoryRepository;
+use App\Repositories\Interfaces\UserAuthenticationInterface;
+use App\Repositories\AuthRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +29,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
+        $this->app->bind(ProductRepositoryInterface::class, ProductRepository::class);
+        $this->app->bind(OrderRepositoryInterface::class, OrderRepository::class);
+        $this->app->bind(OrderItemRepositoryInterface::class, OrderItemRepository::class);
+        $this->app->bind(CategoryRepositoryInterface::class, CategoryRepository::class);
+        $this->app->bind(AdminUserAuthenticationInterface::class, AdminAuthRepository::class);
+        $this->app->bind(UserAuthenticationInterface::class, AuthRepository::class);
+        $this->app->bind(UserRegistrationInterface::class, UserRegistrationRepository::class);
+        $this->app->bind(MediaRepositoryInterface::class, MediaRepository::class);
     }
 
     /**
